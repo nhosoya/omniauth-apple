75201d56620b8e595b259aa3b51665ba05c8e2cc
Fabian Jäger <fabian@mailbutler.io>
2019-06-18 22:12:34 +0200

    use JWT gem and some refactor

--- lib/omniauth/strategies/apple.rb
+++ lib/omniauth/strategies/apple.rb
@@ -1,55 +1,61 @@
+# frozen_string_literal: true
+
 require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Apple < OmniAuth::Strategies::OAuth2
-
-      attr_reader :id_token
+      attr_reader :id_info
       args %i[client_id team_id key_id pem]
 
       option :name, 'apple'
-      option :client_options, {
-        site: 'https://appleid.apple.com',
-        authorize_url: '/auth/authorize',
-        token_url: '/auth/token',
-      }
+      option :client_options,
+             site: 'https://appleid.apple.com',
+             authorize_url: '/auth/authorize',
+             token_url: '/auth/token'
 
-      uid { id_token['sub'] }
+      uid { id_info['sub'] }
 
       info do
-        { sub: id_token['sub'] }
+        {
+          sub: id_info['sub'],
+          extra: {
+            raw_info: id_info
+          }
+        }
       end
 
       def client
-        ::OAuth2::Client.new(options.client_id, client_secret, deep_symbolize(options.client_options))
+        OAuth2::Client.new(options.client_id, client_secret, deep_symbolize(options.client_options))
       end
 
       def callback_url
-        full_host + script_name + callback_path
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
 
       def build_access_token
         _access_token = super
-        @id_token = ::JSON::JWT.decode(_access_token.params['id_token'], :skip_verification)
+        @id_info = JWT.decode(_access_token.params['id_token'], nil, false)
         _access_token
       end
 
       private
 
       def client_secret
-        jwt = ::JSON::JWT.new(
+        payload = {
           iss: options.team_id,
           aud: 'https://appleid.apple.com',
           sub: options.client_id,
           iat: Time.current,
           exp: 1.minutes.after
-        )
-        jwt.kid = options.key_id
-        jwt.sign(private_key).to_s
+        }
+        headers = { kid: options.key_id }
+
+        JWT.encode(payload, private_key, 'ES256', headers)
       end
 
       def private_key
-        ::OpenSSL::PKey::EC.new(options.pem)
+        OpenSSL::PKey::EC.new(options.pem)
       end
     end
   end
