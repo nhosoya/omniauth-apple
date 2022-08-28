d5093dbf99a767f906c414315370fc85ff4756b1
thehungrycoder <rajuru@gmail.com>
2022-08-28 11:50:19 -0400

    skip iat verify

--- lib/omniauth/strategies/apple.rb
+++ lib/omniauth/strategies/apple.rb
@@ -77,7 +77,7 @@ module OmniAuth
                        jwt_options = {
                          verify_iss: true,
                          iss: 'https://appleid.apple.com',
-                         verify_iat: true,
+                         verify_iat: false,
                          verify_aud: true,
                          aud: [options.client_id].concat(options.authorized_client_ids),
                          algorithms: ['RS256'],
