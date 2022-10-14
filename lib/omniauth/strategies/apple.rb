92ac3ecafef9ea6ee4d0f1126ef4b2027a079c88
Brad Gessler <bradgessler@gmail.com>
2022-10-14 15:04:54 -0700

    Fix omniauth-apple compatbility with Apple's decision to POST the response.
    This approach drops the callback_phase block from https://github.com/discourse/discourse-apple-auth/blob/40ef076fa744d562ce54f3f30921a1b387e042fb/lib/omniauth_apple.rb#L60-L72 into this plugin.
    
    It works, but I can't speak for the security of this approach.

--- lib/omniauth/strategies/apple.rb
+++ lib/omniauth/strategies/apple.rb
@@ -61,6 +61,21 @@ module OmniAuth
         options[:redirect_uri] || (full_host + callback_path)
       end
 
+      def callback_phase
+        if request.request_method.downcase.to_sym == :post
+          url = "#{callback_url}"
+
+          if (code = request.params['code']) && (state = request.params['state'])
+            url += "?code=#{CGI::escape(code)}"
+            url += "&state=#{CGI::escape(state)}"
+            url += "&user=#{CGI::escape(request.params['user'])}" if request.params['user']
+          end
+          session.options[:drop] = true # Do not set a session cookie on this response
+          return redirect url
+        end
+        super
+      end
+
       private
 
       def new_nonce
